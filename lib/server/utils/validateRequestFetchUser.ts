512c4dc1fb5817e681d541925044f2d891a3afa8
incogiscool <94598096+incogiscool@users.noreply.github.com>
2024-03-11 12:34:42 +0400

    add critical comment

--- lib/server/utils/validateRequestFetchUser.ts
+++ lib/server/utils/validateRequestFetchUser.ts
@@ -1,12 +1,12 @@
 import { validateRequest } from "../lucia/functions/validateRequest";
 import { UserDocumentsRef } from "../mongo/init";
 
+// CONNECT TO MONGODB BEFORE USING FUNCTION
 export const validateRequestFetchUser = async () => {
   const { user } = await validateRequest();
 
   if (!user) throw new Error("Invalid session.");
 
-  console.log(user);
   const userDoc = await UserDocumentsRef.findOne({ _id: user.id });
 
   if (!userDoc) throw new Error("Error fetching user document.");
