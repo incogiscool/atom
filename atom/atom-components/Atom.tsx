450289628d0252e7ecd8f204213493bc05cd0d4a
incogiscool <adam546944@gmail.com>
2024-03-14 15:38:43 +0400

    fixed mdx parsing

--- atom/atom-components/Atom.tsx
+++ atom/atom-components/Atom.tsx
@@ -28,33 +28,35 @@ export const Atom = async ({
     <>
       {res ? (
         <main className="flex justify-center sm:p-20 p-8">
-          <article className="flex items-center flex-col gap-4 max-w-[950px]">
-            <section className="space-y-6">
-              <header className="space-y-4">
-                <h1 className="sm:text-5xl text-4xl text-center font-bold">
-                  {res.title}
-                </h1>
+          <article className="prose lg:prose-xl">
+            <div className="flex items-center flex-col gap-4 max-w-[950px]">
+              <section className="space-y-6">
+                <header className="space-y-4">
+                  <h1 className="sm:text-5xl text-4xl text-center font-bold">
+                    {res.title}
+                  </h1>
 
-                <div className="flex items-center justify-center gap-4">
-                  <time dateTime={publish_date.toDateString()}>
-                    {publish_date.toDateString()}
-                  </time>
-                  |<p className="author">{res.author}</p>
-                </div>
-              </header>
+                  <div className="flex items-center justify-center gap-4">
+                    <time dateTime={publish_date.toDateString()}>
+                      {publish_date.toDateString()}
+                    </time>
+                    |<p className="author">{res.author}</p>
+                  </div>
+                </header>
 
-              {res.image && (
-                <img
-                  alt="article-image"
-                  src={res.image}
-                  className="w-full h-auto rounded-xl"
-                />
-              )}
-            </section>
+                {res.image && (
+                  <img
+                    alt="article-image"
+                    src={res.image}
+                    className="w-full h-auto rounded-xl"
+                  />
+                )}
+              </section>
 
-            <section className="mt-12">
-              <AtomBody className="space-y-6" body={res.body} />
-            </section>
+              <section className="mt-12">
+                <AtomBody className="space-y-6" body={res.body} />
+              </section>
+            </div>
           </article>
         </main>
       ) : (
--- atom/atom-components/AtomBody.tsx
+++ atom/atom-components/AtomBody.tsx
@@ -1,4 +1,6 @@
 import { MDXRemote } from "next-mdx-remote/rsc";
+import sanitizeHtml from "sanitize-html";
+import { marked } from "marked";
 
 export const AtomBody = async ({
   className,
@@ -7,47 +9,50 @@ export const AtomBody = async ({
   className?: string;
   body: string;
 }) => {
+  const sanitized = sanitizeHtml(body);
+  const parsed = await marked(sanitized);
+
   return (
     <div className={className}>
       <MDXRemote
-        components={{
-          h1: (props) => (
-            <h1 {...props} className="text-4xl font-bold">
-              {props.children}
-            </h1>
-          ),
-          h2: (props) => (
-            <h2 {...props} className="text-3xl font-bold">
-              {props.children}
-            </h2>
-          ),
-          h3: (props) => (
-            <h3 {...props} className="text-2xl font-semibold">
-              {props.children}
-            </h3>
-          ),
-          h4: (props) => (
-            <h3 {...props} className="text-xl font-semibold">
-              {props.children}
-            </h3>
-          ),
-          h5: (props) => (
-            <h3 {...props} className="text-lg font-semibold">
-              {props.children}
-            </h3>
-          ),
-          h6: (props) => (
-            <h3 {...props} className="text-sm font-semibold">
-              {props.children}
-            </h3>
-          ),
-          a: (props) => (
-            <a {...props} className="text-blue-600 underline">
-              {props.children}
-            </a>
-          ),
-        }}
-        source={body || "no text"}
+        // components={{
+        //   h1: (props) => (
+        //     <h1 {...props} className="text-4xl font-bold">
+        //       {props.children}
+        //     </h1>
+        //   ),
+        //   h2: (props) => (
+        //     <h2 {...props} className="text-3xl font-bold">
+        //       {props.children}
+        //     </h2>
+        //   ),
+        //   h3: (props) => (
+        //     <h3 {...props} className="text-2xl font-semibold">
+        //       {props.children}
+        //     </h3>
+        //   ),
+        //   h4: (props) => (
+        //     <h3 {...props} className="text-xl font-semibold">
+        //       {props.children}
+        //     </h3>
+        //   ),
+        //   h5: (props) => (
+        //     <h3 {...props} className="text-lg font-semibold">
+        //       {props.children}
+        //     </h3>
+        //   ),
+        //   h6: (props) => (
+        //     <h3 {...props} className="text-sm font-semibold">
+        //       {props.children}
+        //     </h3>
+        //   ),
+        //   a: (props) => (
+        //     <a {...props} className="text-blue-600 underline">
+        //       {props.children}
+        //     </a>
+        //   ),
+        // }}
+        source={parsed || "no text"}
       />
     </div>
   );
