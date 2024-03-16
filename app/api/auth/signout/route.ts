9bb46a5bd2d3f2b7034f18fd825ba7f806d8146e
incogiscool <adam546944@gmail.com>
2024-03-16 12:16:44 +0400

    added signout

---
+++ app/api/auth/signout/route.ts
@@ -0,0 +1,42 @@
+import { NextRequest, NextResponse } from "next/server";
+import { ApiResponse } from "../signup/route";
+import { connectToDatabase } from "@/lib/server/mongo/init";
+import { validateRequest } from "@/lib/server/lucia/functions/validateRequest";
+import { lucia } from "@/lib/server/lucia/init";
+import { cookies } from "next/headers";
+
+export const POST = async (request: Request) => {
+  try {
+    await connectToDatabase();
+
+    const { session } = await validateRequest();
+    if (!session) {
+      return {
+        error: "Unauthorized",
+      };
+    }
+
+    await lucia.invalidateSession(session.id);
+
+    const sessionCookie = lucia.createBlankSessionCookie();
+    cookies().set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes
+    );
+
+    return NextResponse.json<ApiResponse>({
+      success: true,
+      message: "Successfuly signed out user.",
+      response: null,
+    });
+  } catch (err: any) {
+    console.log(err);
+
+    return NextResponse.json<ApiResponse>({
+      response: null,
+      message: err.message || err,
+      success: false,
+    });
+  }
+};
--- components/sidebars/AppSidebarNav.tsx
+++ components/sidebars/AppSidebarNav.tsx
@@ -3,6 +3,14 @@ import LogoBlack from "@/public/atom-black.svg";
 import Link from "next/link";
 import { navOptions } from "@/lib/contants";
 import { NavOptionIds } from "@/lib/types";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { signoutUser } from "@/lib/client/auth/signoutUser";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 export const AppSidebarNav = ({
   active,
@@ -13,6 +21,20 @@ export const AppSidebarNav = ({
   email: string;
   name: string;
 }) => {
+  const router = useRouter();
+
+  async function signout() {
+    try {
+      await signoutUser();
+
+      router.push("/signin");
+    } catch (err: any) {
+      console.log(err);
+
+      toast.error(err.message || err);
+    }
+  }
+
   return (
     <nav className="flex flex-col justify-between items-center h-screen p-6 py-12 w-[250px] border-r">
       <div className="flex flex-col items-center w-full">
@@ -33,15 +55,24 @@ export const AppSidebarNav = ({
         </div>
       </div>
 
-      <Link
-        href={"/app/settings"}
-        className="w-full border hover:bg-slate-100 transition p-3 rounded-lg text-slate-800"
-      >
-        <p className="font-medium text-sm">{name}</p>
-        <p className="text-slate-800 text-[12px] text-wrap break-all">
-          {email}
-        </p>
-      </Link>
+      <Popover>
+        <PopoverTrigger>
+          <div className="w-full border text-left hover:bg-slate-100 transition p-3 rounded-lg text-slate-800">
+            <p className="font-medium text-sm">{name}</p>
+            <p className="text-slate-800 text-[12px] text-wrap break-all">
+              {email}
+            </p>
+          </div>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px]">
+          <p
+            className="w-full hover:bg-slate-100 rounded-lg transition p-1 px-2 cursor-pointer"
+            onClick={signout}
+          >
+            Sign out
+          </p>
+        </PopoverContent>
+      </Popover>
     </nav>
   );
 };
