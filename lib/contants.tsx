eb3c85c425732f01b1da6fe06e93fabcfe792bfa
incogiscool <94598096+incogiscool@users.noreply.github.com>
2024-03-16 23:51:44 +0400

    add plans details

--- lib/contants.tsx
+++ lib/contants.tsx
@@ -1,5 +1,6 @@
 import { GoDatabase } from "react-icons/go";
 import { IoSettingsOutline, IoWalletOutline } from "react-icons/io5";
+import { Plan } from "./types";
 
 export const plans = ["free", "business"] as const;
 
@@ -30,4 +31,22 @@ export const projectTitleMaxLength = 50;
 export const baseAPIRoute = "http://localhost:3000/api";
 export const mongoDBURI = process.env.MONGO_DB_URI;
 
-// exort cons
+export const planDetails: {
+  title: string;
+  id: Plan;
+  price: number | null;
+  description: string;
+}[] = [
+  {
+    title: "Free",
+    id: "free",
+    price: null,
+    description: "idk tis is the free pla",
+  },
+  {
+    title: "Business",
+    id: "business",
+    price: 12,
+    description: "bruh thsi the busiens plan",
+  },
+];
