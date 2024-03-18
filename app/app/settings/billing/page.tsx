df8c2d197e13850d86e60756b26d8faf86b89a17
incogiscool <94598096+incogiscool@users.noreply.github.com>
2024-03-18 11:17:10 +0400

    init billing page

---
+++ app/app/settings/billing/page.tsx
@@ -0,0 +1,26 @@
+import { AppContainer } from "@/components/containers/AppContainer";
+import { fetchUser } from "@/lib/server/functions/user/fetchUser";
+
+const Page = async () => {
+  const { success, message, response: userDocument } = await fetchUser();
+
+  return (
+    <>
+      {success ? (
+        <AppContainer
+          email={userDocument.email}
+          first_name={userDocument.first_name}
+          active="billing"
+        >
+          <h1 className="font-semibold text-4xl">Billing</h1>
+
+          <div className="mt-12"></div>
+        </AppContainer>
+      ) : (
+        message
+      )}
+    </>
+  );
+};
+
+export default Page;
