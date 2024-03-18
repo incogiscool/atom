fb5aed46d19a3874567dfc48228dc47371c93609
incogiscool <94598096+incogiscool@users.noreply.github.com>
2024-03-18 11:26:01 +0400

    add simple footer

--- components/containers/MainContainer.tsx
+++ components/containers/MainContainer.tsx
@@ -1,13 +1,54 @@
 import { ReactNode } from "react";
 import { Navbar } from "../nav/Navbar";
+import Image from "next/image";
+import LogoBlack from "@/public/atom-black.svg";
+import Link from "next/link";
 
 export const MainContainer = ({ children }: { children: ReactNode }) => {
+  const footerOptions = [
+    {
+      title: "Get Started",
+      link: "/get-started",
+    },
+    {
+      title: "Pricing",
+      link: "/pricing",
+    },
+    {
+      title: "Blog",
+      link: "/blog",
+    },
+    {
+      title: "Sign in",
+      link: "/signin",
+    },
+    {
+      title: "Sign up",
+      link: "/signup",
+    },
+  ];
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <div className="px-20 p-8 pt-36 pb-12">
         <main className="h-full flex-1">{children}</main>
-        <footer>footer</footer>
+        <footer className="h-[100px] bg-black gap-8 p-4 rounded-full flex items-center justify-center">
+          <Image
+            src={LogoBlack}
+            width={50}
+            height={50}
+            alt="logo-black"
+            className="invert"
+          />
+          <ul className="flex gap-4">
+            {footerOptions.map((option) => (
+              <Link href={option.link}>
+                <li className="text-white">{option.title}</li>
+              </Link>
+            ))}
+          </ul>
+        </footer>
       </div>
     </div>
   );
