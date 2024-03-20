c2821fb4588ccc5f865201366987f4c387c85ace
incogiscool <94598096+incogiscool@users.noreply.github.com>
2024-03-20 12:34:15 +0400

    change to MyAppContainer

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ const atomBlogComponentString = `// app/blog/[id]/page.tsx
 
 import { Atom } from "@/atom/atom-components/Atom";
 import { generatePostMetadata } from "@/atom/lib/client/generatePostMetadata";
-import { MainContainer } from "@/...";
+import { MyAppContainer } from "@/...";
 
 export type BlogParams = { params: { id: string } };
 
@@ -42,9 +42,9 @@ export const generateMetadata = async ({ params }: BlogParams) => {
 
 export default async function BlogPage({ params }: BlogParams) {
   return (
-    <MainContainer>
+    <MyAppContainer>
       <Atom apiKey={process.env.TEST_API_KEY!} postId={params.id} />
-    </MainContainer>
+    </MyAppContainer>
   );
 }
 `;
