5bdd31ee5505a0fe9ed8ce44382298f854aa57e5
incogiscool <94598096+incogiscool@users.noreply.github.com>
2024-03-21 13:00:06 +0400

    start middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,13 @@
 import { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
-export default function middleware(request: NextRequest) {}
+export default function middleware(request: NextRequest) {
+  const requestPath = request.nextUrl.pathname;
+  const protectedPaths = ["/app"];
+  const sensitivePaths = ["/signup", "/signin"];
+
+  //add rate limiting and protect paths
+}
 
 // See "Matching Paths" below to learn more
 export const config = {
