a13e1fb97320c3a5c96ac6737ea35a111d70982f
incogiscool <94598096+incogiscool@users.noreply.github.com>
2025-11-27 12:18:41 -0500

    Re-add sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,10 +2,10 @@ import { MetadataRoute } from "next";
 import { generateSitemap } from "atom-nextjs";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  // const routes = await generateSitemap(
-  //   process.env.ATOM_PROJECT_KEY!,
-  //   "https://atomcms.vercel.app/blog"
-  // );
+  const routes = await generateSitemap(
+    process.env.ATOM_PROJECT_KEY!,
+    "https://atomcms.vercel.app/blog"
+  );
 
   return [
     {
@@ -13,6 +13,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       lastModified: new Date(),
       priority: 0.7,
     },
-    // ...routes,
+    ...routes,
   ];
 }
